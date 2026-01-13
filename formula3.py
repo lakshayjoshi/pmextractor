6fc62d868e96c4603d9d02814581b5cd995edda9
Lakshay Joshi <59448205+lakshayjoshi@users.noreply.github.com>
2026-01-13 20:41:29 +0530

    Create formula3.py
    v1.3 - Works with '.swagger' format with folders.

---
+++ formula3.py
@@ -0,0 +1,13 @@
+import json
+ 
+def extract_urls(swagger_file):
+    with open(swagger_file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    paths = data.get("paths", {}).keys()
+    print("Extracted API URLs:")
+    for path in paths:
+        print(path)
+ 
+# Replace 'swagger.json' with the actual file name
+swagger_file = "swagger (1) (2).json"
+extract_urls(swagger_file)
